f70507c867897bc8c4e3873d0b44c1f87b50893e
fkonkol <fp.konkol@gmail.com>
2025-01-13 07:07:27 +0100

    add shape and its sizes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,64 @@
 #include <ncurses.h>
 
+struct Vec2
+{
+    int x, y;
+
+    Vec2() : x(0), y(0) {}
+};
+
+class Shape
+{
+public:
+    enum class Size
+    {
+        Small, Medium, Large,
+    };
+
+    Size GetSize()
+    {
+        return m_Size;
+    }
+
+    void ScaleUp()
+    {
+        if (m_Size < Shape::Size::Large)
+        {
+            m_Size = static_cast<Size>(static_cast<int>(m_Size) + 1);
+        }
+    }
+
+    void ScaleDown()
+    {
+        if (m_Size > Shape::Size::Small)
+        {
+            m_Size = static_cast<Size>(static_cast<int>(m_Size) - 1);
+        }
+    }
+
+    void Draw()
+    {
+        switch (m_Size)
+        {
+        case Size::Small:
+            printw("Small");
+            break;
+        case Size::Medium:
+            printw("Medium");
+            break;
+        case Size::Large:
+            printw("Large");
+            break;
+        default:
+            break;
+        }
+    }
+
+private:
+    Size m_Size = Size::Medium;
+    Vec2 m_Position;
+};
+
 class Application
 {
 public:
@@ -27,9 +86,34 @@ public:
 
     void Run()
     {
-        printw("Hello");
-        getch();
+        while (true)
+        {
+            clear();
+            m_Shape.Draw();
+            refresh();
+
+            int key = getch();
+
+            if (key == 'q') {
+                break;
+            }
+
+            switch (key)
+            {
+            case '+':
+                m_Shape.ScaleUp();
+                break;
+            case '-':
+                m_Shape.ScaleDown();
+                break;
+            default:
+                break;
+            }
+        }
     }
+
+private:
+    Shape m_Shape;
 };
 
 int main()
