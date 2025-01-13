bcceb982857807b25700f5123d127d4e0f282034
fkonkol <fp.konkol@gmail.com>
2025-01-13 07:23:51 +0100

    draw shape

--- main.cpp
+++ main.cpp
@@ -41,13 +41,37 @@ public:
         switch (m_Size)
         {
         case Size::Small:
-            mvprintw(m_Position.y, m_Position.x, "Small");
+            mvprintw(m_Position.y, m_Position.x, "##################");
+            mvprintw(m_Position.y + 1, m_Position.x, "##");
+            mvprintw(m_Position.y + 2, m_Position.x, "  ##");
+            mvprintw(m_Position.y + 3, m_Position.x, "    ##");
+            mvprintw(m_Position.y + 4, m_Position.x, "  ##");
+            mvprintw(m_Position.y + 5, m_Position.x, "##");
+            mvprintw(m_Position.y + 6, m_Position.x, "##################");
             break;
         case Size::Medium:
-            mvprintw(m_Position.y, m_Position.x, "Medium");
+            mvprintw(m_Position.y, m_Position.x, "###########################");
+            mvprintw(m_Position.y + 1, m_Position.x, "###");
+            mvprintw(m_Position.y + 2, m_Position.x, "   ###");
+            mvprintw(m_Position.y + 3, m_Position.x, "      ###");
+            mvprintw(m_Position.y + 4, m_Position.x, "         ###");
+            mvprintw(m_Position.y + 5, m_Position.x, "      ###");
+            mvprintw(m_Position.y + 6, m_Position.x, "   ###");
+            mvprintw(m_Position.y + 7, m_Position.x, "###");
+            mvprintw(m_Position.y + 8, m_Position.x, "###########################");
             break;
         case Size::Large:
-            mvprintw(m_Position.y, m_Position.x, "Large");
+            mvprintw(m_Position.y, m_Position.x, "#########################################");
+            mvprintw(m_Position.y + 1, m_Position.x, "####");
+            mvprintw(m_Position.y + 2, m_Position.x, "    ####");
+            mvprintw(m_Position.y + 3, m_Position.x, "        ####");
+            mvprintw(m_Position.y + 4, m_Position.x, "            ####");
+            mvprintw(m_Position.y + 5, m_Position.x, "                ####");
+            mvprintw(m_Position.y + 6, m_Position.x, "            ####");
+            mvprintw(m_Position.y + 7, m_Position.x, "        ####");
+            mvprintw(m_Position.y + 8, m_Position.x, "    ####");
+            mvprintw(m_Position.y + 9, m_Position.x, "####");
+            mvprintw(m_Position.y + 10, m_Position.x, "#########################################");
             break;
         default:
             break;
