0c4015930cf3e8dbcb597bb9a8dd67369215a8aa
fkonkol <fp.konkol@gmail.com>
2025-01-13 06:56:47 +0100

    add application class

--- main.cpp
+++ main.cpp
@@ -1,15 +1,39 @@
 #include <ncurses.h>
 
+class Application
+{
+public:
+    Application() {
+        Initialize();
+    }
+
+    ~Application() {
+        Clean();
+    }
+
+    void Initialize()
+    {
+        initscr();
+        cbreak();
+        noecho();
+        keypad(stdscr, TRUE);
+        curs_set(FALSE);
+    }
+
+    void Clean()
+    {
+        endwin();
+    }
+
+    void Run()
+    {
+        printw("Hello");
+        getch();
+    }
+};
+
 int main()
 {
-    initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);
-    curs_set(FALSE);
-
-    printw("Hello");
-
-    getch();
-    endwin();
+    Application application;
+    application.Run();
 }
